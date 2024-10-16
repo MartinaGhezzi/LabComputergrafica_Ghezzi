1b3e4f6bdbde51a51a32e8dc6c7e5881b985cb96
MartinaGhezzi <martina1.ghezzi@mail.polimi.it>
2024-10-16 10:00:07 +0200

    esercizio follow_the_mouse

--- lezione 3/Followthemouse/sketch.js
+++ lezione 3/Followthemouse/sketch.js
@@ -1,30 +1,32 @@
 //scope globale, le variabili hanno visibilità su entra,be le funzioni//
-let canvasXMas=400
-let canvasYMas=400
+let canvasXMas=400;
+let canvasYMas=400;
+
 function setup() {
   createCanvas(canvasXMas, canvasYMas);
-  background (200)
+  background (220);
 }
 
 function draw() {
   textSize(15);
-  let string_toprint="Contenuto mouseX" +mouseX + "y=" +mouseY;
+  let string_toprint="Contenuto mouseX" +mouseX ;
   //  text(string_toprint, 20, 20);
-
-  strokeWeight(2);
+  ",y=" +mouseY;
+  strokeWeight(1);
   line (0, mouseX, canvasXMas, mouseY);
   //feedback tasto mouse sinistro
   //cambiare colore della line se schiacciamo il mouse
   //p5 --> true, se il mouse è schiacciato
   //altrimenti, è false
   if(mouseIsPressed == true) {
-    if(mouseButton==RIGHT) {
-      background(220)
+    if(mouseButton == RIGHT) {
+      background(220);
     }
       //coloriamo di giallo
       stroke("yellow");
   } else {
-    stroke("black")
+    //altrimenti nero
+    stroke("black");
   }
-  //altrimenti nero
+  mouseIsPressed;
 }
